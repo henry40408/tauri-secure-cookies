d9b8f662a5ca0de1d9809c29629c82811095874f
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2022-05-03 21:13:05 +0800

    feat: simple HTTPS server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::any().map(|| format!("OK"));
+    warp::serve(hello)
+        .tls()
+        .cert_path("localhost.pem")
+        .key_path("localhost-key.pem")
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
