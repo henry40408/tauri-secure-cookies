727beae3185b475c81d334193d2773dbd8df6984
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2022-05-10 20:52:21 +0800

    feat: use webcontext to preserve cookies

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,17 +1,24 @@
 use wry::application::event::{Event, StartCause, WindowEvent};
 use wry::application::event_loop::{ControlFlow, EventLoop};
 use wry::application::window::WindowBuilder;
-use wry::webview::WebViewBuilder;
+use wry::webview::{WebContext, WebViewBuilder};
 
 fn main() -> anyhow::Result<()> {
+    let mut current_dir = std::env::current_dir()?;
+    current_dir.push("data");
+    dbg!(&current_dir);
+
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("Hello World")
         .build(&event_loop)?;
 
+    let mut webcontext = WebContext::new(Some(current_dir));
+
     let webview = WebViewBuilder::new(window)?
         .with_url("https://localhost:3000")?
         .with_devtools(true)
+        .with_web_context(&mut webcontext)
         .build()?;
 
     webview.open_devtools();
